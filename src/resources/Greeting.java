53abccf1c036bac101f252baaebc96354727557e
quikhennie <hennie@mystemsoftware.com>
2015-02-12 23:40:17 +0200

    Initial Commit
    Commit all files

---
+++ src/resources/Greeting.java
@@ -0,0 +1,16 @@
+package resources;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@Path("/hr/")
+public class Greeting {
+
+	@GET
+	@Produces("text/plain")
+	@Path("employee")
+	public String getEmployee() {
+		return "What World";
+	}
+		
+}
